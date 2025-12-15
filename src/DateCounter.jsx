bc7acf54236dac132f979fe8da632c731850fe6a
Samuel Harpaz <sharpaz24@gmail.com>
2025-12-15 17:37:57 -0500

    transfer state management to useReducer

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,14 +1,31 @@
 import { useReducer, useState } from 'react';
 
+const initialState = {
+  count: 0,
+  step: 1
+};
+
 function reducer(state, action) {
-  if (action.type === 'dec') return state - 1;
-  if (action.type === 'inc') return state + 1;
-  if (action.type === 'setCount') return action.payload;
+  switch (action.type) {
+    case 'dec':
+      return { ...state, count: state.count - state.step };
+    case 'inc':
+      return { ...state, count: state.count + state.step };
+    case 'setCount':
+      return { ...state, count: action.payload };
+    case 'setStep':
+      return { ...state, step: action.payload };
+    case 'reset':
+      return initialState;
+    default:
+      throw new Error('Cannot find action');
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
@@ -31,19 +48,23 @@ function DateCounter() {
     dispatch({ type: 'setCount', payload: +e.target.value });
   };
 
-  const defineStep = function (e) {
-    setStep(Number(e.target.value));
-  };
-
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    // setCount(0);
+    // setStep(1);
+
+    dispatch({ type: 'reset' });
   };
 
   return (
     <div className="counter">
       <div>
-        <input type="range" min="0" max="10" value={step} onChange={defineStep} />
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={e => dispatch({ type: 'setStep', payload: +e.target.value })}
+        />
         <span>{step}</span>
       </div>
 
