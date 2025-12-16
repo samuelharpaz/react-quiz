4ca1a0c41a7317c795fab5f9c74dba7fdfd39b92
Samuel Harpaz <sharpaz24@gmail.com>
2025-12-15 19:02:30 -0500

    set up for quiz app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import MainEl from './MainEl';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <MainEl>
+        <p>1/15</p>
+        <p>Question</p>
+      </MainEl>
     </div>
   );
 }
---
+++ src/MainEl.jsx
@@ -0,0 +1,5 @@
+function MainEl({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+export default MainEl;
